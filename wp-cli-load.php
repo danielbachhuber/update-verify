1e73f5e0065db1f2656487cfcfab69f9bfd4c77b
Daniel Bachhuber <daniel@handbuilt.co>
2017-08-14 15:21:54 -0700

    Clean up WPCS issues

--- wp-cli-load.php
+++ wp-cli-load.php
@@ -1,9 +1,14 @@
 <?php
+/**
+ * Loads the plugin when called from the WP-CLI context
+ *
+ * @package Update-Verify
+ */
 
 if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-WP_CLI::add_wp_hook( 'plugins_loaded', function(){
+WP_CLI::add_wp_hook( 'plugins_loaded', function() {
 	require_once dirname( __FILE__ ) . '/update-verify.php';
 });
