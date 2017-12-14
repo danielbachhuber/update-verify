4599c1ef7d6531e302f32d956a1fde7ca3b97a4b
Daniel Bachhuber <daniel@handbuilt.co>
2017-12-14 13:19:10 -0800

    Fix PHPCS issues

--- register-command.php
+++ register-command.php
@@ -13,15 +13,17 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	);
 
 	// Hack because WP-CLI doesn't like commands registered to 'core' that run on before_wp_load
-	WP_CLI::add_hook( 'find_command_to_run_pre', function(){
-		WP_CLI::add_command(
-			'core safe-update', array( 'UpdateVerify\CLI', 'safe_update' ), array(
-				'before_invoke' => function() {
-					if ( ! version_compare( WP_CLI_VERSION, '1.5.0-alpha', '>=' ) ) {
-						WP_CLI::error( 'Safe update requires WP-CLI 1.5.0-alpha-d71d228 or later' );
-					}
-				},
-			)
-		);
-	});
+	WP_CLI::add_hook(
+		'find_command_to_run_pre', function() {
+			WP_CLI::add_command(
+				'core safe-update', array( 'UpdateVerify\CLI', 'safe_update' ), array(
+					'before_invoke' => function() {
+						if ( ! version_compare( WP_CLI_VERSION, '1.5.0-alpha', '>=' ) ) {
+							WP_CLI::error( 'Safe update requires WP-CLI 1.5.0-alpha-d71d228 or later' );
+						}
+					},
+				)
+			);
+		}
+	);
 }
